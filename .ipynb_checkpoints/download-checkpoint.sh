45f8fe63c388a01b413a7d9ac4487664f02a1101
Albert Mundu <albert.adel007@gmail.com>
2020-09-23 00:29:10 +0530

    added download.sh

--- .ipynb_checkpoints/download-checkpoint.sh
+++ .ipynb_checkpoints/download-checkpoint.sh
@@ -2,9 +2,9 @@
 
 wget http://chaladze.com/l5/img/Linnaeus%205%20256X256.rar
 
-unrar x "Linnaeus 5 256x256.rar"
+unrar x "Linnaeus 5 256X256.rar"
 
-mv "Linnaeus 5 256x256" data
+mv "Linnaeus 5 256X256" data
 
 cd data
 
--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# wget http://chaladze.com/l5/img/Linnaeus%205%20256X256.rar
+wget http://chaladze.com/l5/img/Linnaeus%205%20256X256.rar
 
 unrar x "Linnaeus 5 256X256.rar"
 
